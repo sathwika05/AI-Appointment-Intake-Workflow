5081f35ee1c8d15ef436f548770ab68fe6440b12
sathwika05 <sathwikaparshaboina05@gmail.com>
2026-01-26 16:36:05 -0500

    changed url

--- code/AI-Intake/frontend/frontend-ui/src/app/(dashboard)/admin/page.tsx
+++ code/AI-Intake/frontend/frontend-ui/src/app/(dashboard)/admin/page.tsx
@@ -4,7 +4,7 @@ import UserCard from "@/components/UserCard";
 import {useEffect, useState} from "react";
 
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3001/ai-intake";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:3001";
 
 
 type Counts = {
